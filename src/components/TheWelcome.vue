357070ca74f4aab842775b42c7c8a3c21fffcc70
RanatungeDeshan <Chithranga.Ranatunge@Student.HTW-Berlin.de>
2023-11-16 00:05:59 +0100

    3-milestone

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Divide and Conquer</template>
+
+    Gooooo.. get it ..
+
+    <br />
+
+    More instructions are available..
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Intelligence Hub</template>
+
+   Why so serious..
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Hot Shots</template>
+
+    Trust no one
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Bringing Our A Game</template>
+
+   Bring it on..
+  </WelcomeItem>
+</template>
