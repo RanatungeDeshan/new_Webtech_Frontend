46ef09db6041f239d55b92cc6ef5275ec3cbd30c
Yannick <yannick.schulz13@outlook.de>
2023-12-03 21:31:03 +0100

    Added connection between Frontend and Backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <DynamicForm title="Welcome to your Vue.js App"/>
   </main>
 </template>
+
+<script setup lang="ts">
+import type HelloWorldVue from '../components/HelloWorld.vue';
+import DynamicForm from '../components/HelloWorld.vue'
+</script>
+
+<style scoped>
+
+</style>
+
